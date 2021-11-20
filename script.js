c8daddd16329cac50be31d2289afae31f48e8af1
Yusheng Wu <wuyusheng1023@gmail.com>
2021-11-20 14:17:07 +0200

    add two new upgrades

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 var n = 0;
+var cpc = 1;
 
-function plus_one() {
-	n++;
+function plusOne() {
+	n += cpc;
 	document.getElementById("coockies").innerHTML = `${n} cookies`;
+}
+
+function reinforcedIndexFinger() {
+	cpc = 2;
+	document.getElementById("upgrade001").style.visibility = 'hidden';
+	document.getElementById("upgrade002").style.visibility = 'visible';
+}
+
+function carpalTunnelPreventionCream() {
+	cpc = 4;
+	document.getElementById("upgrade002").style.visibility = 'hidden';
 }
